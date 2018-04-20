77dfcd8e2b7696791388ad01400d2aca28d6cf97
hark130 <hark130@yahoo.com>
2018-04-20 11:20:35 -0500

    master: Cleaned up renderomatic
    Completed argument parsing
    Fixed invalid code
    Modified filename argument into a list of filenames
    Added logic to validate all of the arguments, to include the presence of files

--- renderomatic.py
+++ renderomatic.py
@@ -1,51 +1,80 @@
-import argparse
+from argparse import ArgumentParser
+import os
 
 
 def parse_arguments():
     '''
         Input - None
-        Output - Command line argument list from ParseArgument object
+        Output - Command line argument list from ArgumentParser object
     '''
     # Parser object
-    parser = ParseArgument()
+    parser = ArgumentParser()
     
     # Command line arguments
-    parser.add_argument("-f", "--file", required = True, action = "store_true", help = "Input art file")
-    parser.add_argument("-s", "--sizes", required = True, action = "store_true", help = "List of sizes to render in")
-    parser.add_argument("-m", "--molds", required = True, action = "store_true", help = "List of mouldings to render in")
+    parser.add_argument("-f", "--file", type = str, required = True, help = "Input art file")
+    parser.add_argument("-s", "--sizes", type = str, required = True, help = "List of sizes to render in")
+    parser.add_argument("-m", "--molds", type = str, required = True, help = "List of mouldings to render in")
     
     # List of arguments from the command line
     args = parser.parse_args()
     
-return args
+    return args
 
 
 if __name__ == "__main__":
-    print("We started at the top and now we're here.")
-    
     ### LOCAL VARIABLES ###
-    args  # Parsed arguments
-    filename = ""  # Input art filename parsed from args
+    # Parsed arguments
+    listOfNames = []  # Input art filenames parsed from args
     listOfSizes = []  # List of sizes parsed from args
     listOfMolds = []  # List of moldings parsed from args
+    success = True  # Make this False if anything fails
     
     ### PARSE CLI ARGUMENTS ###
-    args = parse_arguments()
+    args = parse_arguments()  # Parsed arguments
     
-    # Filename
+    # Filenames
     if args.file and args.file.__len__() > 0:
-        filename = args.file
+        listOfNames = args.file.split(",")
     
     # Sizes
     if args.sizes:
-        listOfSizes = args.size.split(",")
+        listOfSizes = args.sizes.split(",")
     
     # Moudlings
     if args.molds:
         listOfMolds = args.molds.split(",")
+
+    ### INPUT VALIDATION ###
+    # Filenames
+    if success:
+        for filename in listOfNames:
+            if filename.__len__() == 0:
+                print("Renderomatic detected an empty filename.\nDid include an extra comma in the -f argument?")
+                success = False
+                break
+            elif not os.path.isfile(filename):
+                print("Renderomatic was unable to locate file '{}' in '{}'.\nWill you please verify the file exists and that its name was type properly?".format(filename, os.getcwd()))
+                success = False
+                break
+    
+    # Sizes
+    if success:
+        for size in listOfSizes:
+            if size.__len__() == 0:
+                print("Renderomatic detected an empty size.\nDid include an extra comma in the -s argument?")
+                success = False
+                break
+
+    # Mouldings
+    if success:
+        for mold in listOfMolds:
+            if mold.__len__() == 0:
+                print("Renderomatic detected an empty moulding.\nDid include an extra comma in the -m argument?")
+                success = False
+                break
         
     ### TEST ###
-    print("Filename:\t{}".format(filename))  # DEBUGGING
+    print("Filename:\t{}".format(listOfNames))  # DEBUGGING
     print("Sizes:   \t{}".format(listOfSizes))  # DEBUGGING
     print("Molds:   \t{}".format(listOfMolds))  # DEBUGGING
         
